771c7e1f92a58cc74c14e5f8e06faefe22c5d3dc
Зайцева Алёна <zaitceva2108@gmail.com>
2026-03-17 04:23:25 +0300

    Add classes: InventoryItem and InventoryManager

---
+++ InventoryApp/InventoryItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace InventoryApp
+{
+    public class InventoryItem
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+
+        public InventoryItem(string name, int quantity, decimal price, string category)
+        {
+            Name = name;
+            Quantity = quantity;
+            Price = price;
+            Category = category;
+        }
+    }
+}
