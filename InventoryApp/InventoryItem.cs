07e054d895db975b446aaf8bd7a2214c47588bff
Зайцева Алёна <zaitceva2108@gmail.com>
2026-05-26 10:10:17 +0300

    Add validation to InventoryItem

--- InventoryApp/InventoryItem.cs
+++ InventoryApp/InventoryItem.cs
@@ -11,6 +11,18 @@ namespace InventoryApp
 
         public InventoryItem(string name, int quantity, decimal price, string category)
         {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Не может быть пустого названия");
+
+            if (quantity < 0)
+                throw new ArgumentException("Не может быть отрицательное количество");
+
+            if (price <= 0)
+                throw new ArgumentException("Цена должна быть больше нуля");
+
+            if (string.IsNullOrWhiteSpace(category))
+                throw new ArgumentException("Не может быть пустой категории");
+
             Name = name;
             Quantity = quantity;
             Price = price;
